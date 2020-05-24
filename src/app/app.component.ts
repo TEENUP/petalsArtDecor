8f15b04c0393c5c169dbc264af94ddc70b4c5d06
TEENUP <puneetgupta5294@gmail.com>
2020-05-25 02:47:11 +0530

    dev: made navbar and footer common to visible on all pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
  
+  
+
+  navbarMenu = [
+    {
+        key: 'home',
+        value: 'Home',
+        redirectTo: '/home'
+    },
+    {
+        key: 'projects',
+        value: 'Projects',
+        redirectTo: '/projects'
+    },
+    {
+        key: 'services',
+        value: 'Services',
+        redirectTo: '/services'
+    },
+    {
+        key: 'contactUs',
+        value: 'Contact Us',
+        redirectTo: '/contact-us'
+    },
+    {
+        key: 'aboutUs',
+        value: 'About Us',
+        redirectTo: '/about-us'
+    },
+]
+
+
 
   constructor() {
   }
