d6bcc5a66109ea918070cae1e8ff3ac5859b80ee
TEENUP <puneetgupta5294@gmail.com>
2020-07-04 20:53:20 +0530

    added send email functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatButtonModule} from '@angular/material/button';
 
 import { ContactUsComponent } from './contact-us/contact-us.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -31,7 +33,9 @@ import { ContactUsComponent } from './contact-us/contact-us.component';
     BrowserAnimationsModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    CommonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,4 +1,7 @@
 import { Component } from "@angular/core";
+import {Email} from "../../assets/js/smtp.js";
+// declare let Email: any;
+
 
 @Component({
     selector: 'contact-us',
@@ -8,7 +11,30 @@ import { Component } from "@angular/core";
 
 export class ContactUsComponent {
 
+    contactData: any = {
+        name:'Puneet',
+        email: 'teenup05@gmail.com',
+        number: '8920226154',
+        msg: 'Hi this is a test email for testing whether email are sent or not... haha',
+    };
+
     constructor() {
 
     }
+
+    sendEmail(contactData) {
+        console.log(contactData);
+        Email.send({
+            Host: 'smtp.elasticemail.com',
+            Username: 'puneetgupta5294@gmail.com',
+            Password: '7B5F3B5E781B48238C5E0F843F6DE23D3BA9',
+            To: 'sagar.singhal94@gmail.com',
+            From: 'puneetgupta5294@gmail.com',
+            Subject: `Query for petals art decor from ${this.contactData.name}`,
+            Body: `
+            <i>This is sent as a Query for Petals art decor.</i> <br/> <b>Name: </b>${this.contactData.name} <br /> <b>Email: </b>${this.contactData.email}<br /> <b>Message: </b>${this.contactData.msg}<br /> <b>Mobile:</b> <br /> ${this.contactData.number} <br><br> <b>~End of Message.~</b> `
+        }).then(message => { alert(message) });
+
+    }
+
 }
