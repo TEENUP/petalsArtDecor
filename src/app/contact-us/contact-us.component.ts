ef14838e5d738d41e946c8fcb48ffeecea6f15da
TEENUP <puneetgupta5294@gmail.com>
2020-07-18 11:33:50 +0530

    style fixes

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -28,12 +28,12 @@ export class ContactUsComponent {
             Host: 'smtp.elasticemail.com',
             Username: 'puneetgupta5294@gmail.com',
             Password: '7B5F3B5E781B48238C5E0F843F6DE23D3BA9',
-            To: 'sagar.singhal94@gmail.com',
+            To: 'info@petalsartdecor.com',
             From: 'puneetgupta5294@gmail.com',
             Subject: `Query for petals art decor from ${this.contactData.name}`,
             Body: `
             <i>This is sent as a Query for Petals art decor.</i> <br/> <b>Name: </b>${this.contactData.name} <br /> <b>Email: </b>${this.contactData.email}<br /> <b>Message: </b>${this.contactData.msg}<br /> <b>Mobile:</b> <br /> ${this.contactData.number} <br><br> <b>~End of Message.~</b> `
-        }).then(message => { alert(message) });
+        }).then(message => { alert('Your Message has been sent successfully') });
 
     }
 
